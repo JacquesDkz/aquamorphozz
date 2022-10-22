56579f5da9ab5cf07a16ddcb3d685379dc620d47
ojoysun <dkz.jacques@gmail.com>
2022-10-22 18:14:27 +0200

    Player

--- Aquamorphozz/Assets/Scripts/PlayerMovement.cs
+++ Aquamorphozz/Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-    public float jumpforce;
+    public float jumpForce;
 
-    private bool isJumping;
-    private bool isGrounded;
+    public bool isJumping;
+    public bool isGrounded;
 
-   // public Transform groundCheckLeft;
-   // public Transform groundCheckRight;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
 
     public Rigidbody2D rb;
     public Animator animator;
@@ -19,27 +22,30 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 velocity = Vector3.zero;
 
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-       // isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
 
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed - Time.deltaTime;
-/*
-        if (Input.GetButtonDown("Jump") && (isGrounded == true))
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
-*/
+
+
         MovePlayer(horizontalMovement);
 
         Flip(rb.velocity.x);
+
         float characterVelocity = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("Speed", characterVelocity);
     }
@@ -49,21 +55,21 @@ public class PlayerMovement : MonoBehaviour
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
-        if (isJumping == true)
+        if(isJumping == true)
         {
-            rb.AddForce(new Vector2(0f, jumpforce));
+            rb.AddForce(new Vector2(0f, jumpForce));
             isJumping = false;
         }
     }
 
     void Flip(float _velocity)
     {
-        if (_velocity > 0.1f)
+        if(_velocity > 0.1f)
         {
             spriteRenderer.flipX = false;
 
         }
-        else if (_velocity < -0.1f)
+        else if(_velocity < 0.1f)
         {
             spriteRenderer.flipX = true;
         }
