c0aa6182dfb87e894cd8c43eccc8073df04fd552
wrixyour <75781244+wrixyour@users.noreply.github.com>
2022-10-22 17:40:21 +0200

    coucocu

--- Aquamorphozz/Assets/Scripts/Hyene.cs
+++ Aquamorphozz/Assets/Scripts/Hyene.cs
@@ -14,6 +14,8 @@ public class Hyene : MonoBehaviour
     private float x;
 
     private int i = 1000;
+
+    private int anim = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,27 +26,63 @@ public class Hyene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(i == 1000)
+        if(!animator.GetBool("Combat"))
         {
-            if(x + walkSpeed * Time.deltaTime < tailleDeplacement && x + walkSpeed * Time.deltaTime > -tailleDeplacement)
+            if(i == 1000)
             {
-                animator.SetBool("walk", true);
-                x += walkSpeed * Time.deltaTime;
-                transform.Translate(-1*Math.Abs(walkSpeed) * Time.deltaTime, 0, 0);
+                if(x + walkSpeed * Time.deltaTime < tailleDeplacement && x + walkSpeed * Time.deltaTime > -tailleDeplacement)
+                {
+                    animator.SetBool("walk", true);
+                    x += walkSpeed * Time.deltaTime;
+                    transform.Translate(-1*Math.Abs(walkSpeed) * Time.deltaTime, 0, 0);
+                }
+                else
+                {
+                    i = 0;
+                }
             }
             else
             {
-                i = 0;
+                animator.SetBool("walk", false);
+                i++;
+                if(i == 1000)
+                {
+                    walkSpeed = -walkSpeed;
+                    transform.Rotate(0, 180, 0, Space.World);
+                }
             }
         }
-        else
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Perso"))
         {
-            animator.SetBool("walk", false);
-            i++;
-            if(i == 1000)
+            anim++;
+            animator.SetBool("Combat", true);
+            if(other.gameObject.transform.position.x < transform.position.x)
             {
-                walkSpeed = -walkSpeed;
-                transform.Rotate(0, 180, 0, Space.World);
+                transform.rotation = new Quaternion(0,0,0,0);
+            }
+            else{
+                transform.rotation = new Quaternion(0,180,0,0);
+            }
+            StartCoroutine(wait(anim));
+        }
+    }
+
+    IEnumerator wait(int i)
+    {
+        yield return new WaitForSeconds((float)1);
+        if(anim == i)
+        {
+            animator.SetBool("Combat", false);
+            if(walkSpeed < 0)
+            {
+                transform.rotation = new Quaternion(0,0,0,0);
+            }
+            else{
+                transform.rotation = new Quaternion(0,180,0,0);
             }
         }
     }
