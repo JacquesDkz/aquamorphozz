8a0170ae3770fe30532cc7b84e404b8a1dc43f01
ojoysun <dkz.jacques@gmail.com>
2022-10-23 15:09:50 +0200

    Definitly fixed Player Moves

--- Aquamorphozz/Assets/Scripts/PlayerMovement.cs
+++ Aquamorphozz/Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,16 @@ public class PlayerMovement : MonoBehaviour
     private bool isJumping;
     private bool isGrounded;
 
-    public Transform groundCheckLeft;
-    public Transform groundCheckRight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
 
     public Rigidbody2D rb;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
 
     private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
 
     // Start is called before the first frame update
     void Start()
@@ -23,23 +25,28 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
 
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
         if (Input.GetButtonDown("Jump") && (isGrounded == true))
         {
             isJumping = true;
         }
 
-        MovePlayer(horizontalMovement);
-
         Flip(rb.velocity.x);
         float characterVelocity = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("Speed", characterVelocity);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        MovePlayer(horizontalMovement);
+
     }
 
     void MovePlayer(float _horizontalMovement)
@@ -65,5 +72,11 @@ public class PlayerMovement : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
